6d87a686ea8c2205a6721d3376c9a7139e5a41d8
Soojong Kim <sujjong456@gmail.com>
2024-03-20 12:21:45 +0900

    0320

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import './modal';
 
+
+{/* 주석???} 다는 법 */}
 function App() {
+
+  let post = 'Testing';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>Test</h4>
+      </div>
+      <h4>{post}</h4> 
     </div>
   );
 }
