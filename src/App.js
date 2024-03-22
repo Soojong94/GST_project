387cff934c5f69d199c8275aba992a5cec947d6f
khbduino <khbroom1120@gmail.com>
2024-03-22 15:24:15 +0900

    모달

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Sidebar3Script from './sidebar-03/sidebar3_script2';
+
 import Modal from './modal';
 
 function App() {
--- src/modal.jsx
+++ src/modal.jsx
@@ -1,27 +1,29 @@
-import React,{useState} from "react";
+import React, { useState } from 'react';
 import './modal.css';
 
-function Modal(){
-    const [isOpen, setIsOpen] = useState(false);
 
-    const toggleModal = () => {
-        setIsOpen(!isOpen);
-    }
+function Modal() {
+  const [isOpen, setIsOpen] = useState(false);
 
-    return(
-        <div onClick={toggleModal} style={{ width: '100px', height: '100px', backgroundColor: 'gray'}}>
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  }
 
-            {isOpen && (
-            <div className="modal" onClick={toggleModal}>
-                <div className="modal-content">
-                    
-                    
-                </div>
-            </div>
+  return (
+    <div id='mdbtn'>
+      <div onClick={toggleModal} style={{ width: '100px', height: '100px', background: 'red' }}>Open Modal Area 1</div>
+      <div onClick={toggleModal} style={{ width: '100px', height: '100px', background: 'green' }}>Open Modal Area 2</div>
 
-            )}
+      {isOpen && (
+        <div className='modal' onClick={toggleModal}>
+          <div className='modal-content'>
+            <h2>Hello, this is a modal</h2>
+            <p>This is the modal content.</p>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Modal;
