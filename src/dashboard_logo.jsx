bc3af99320366d1e43ace43e95c0c7dc29cc27c7
Soojong Kim <sujjong456@gmail.com>
2024-03-22 19:58:40 +0900

    0322 마무리

--- src/dashboard_logo.jsx
+++ src/dashboard_logo.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import dashboardImage from './dashboardImg/KakaoTalk_20240318_165848557.png'; 
+import dashboardImage2 from './dashboardImg/KakaoTalk_20240318_165848557_01.png'; 
 import './dashboard_logo.css';
 
 export default function GSTMenu() {
   return (
-    <a href='#' className='menu-link'>
-      <img className='gst-menu' src={dashboardImage} alt="Dashboard Image" />
-    </a>
+    <div>
+      <a href='#' className='menu-link'>
+        <img className='gst-menu' src={dashboardImage2} alt="Dashboard Image" />
+      </a>
+    </div>
   );
 }
