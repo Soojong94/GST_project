f7664976d57f871762b54a0391c63f731e2eba2a
LeeKyunHo <rbsgh369@gmail..com>
2024-03-26 16:07:00 +0900

    실시간 업데이트

--- back/intervalUpdate.js
+++ back/intervalUpdate.js
@@ -3,14 +3,18 @@ const mysqlConnection = require('./mysql');
 const createJson = require('./createJson');
 const update = require('./update');
 const fs = require('fs');
-
-// createJson 함수 실행 후 10분 후에 update 함수 실행
-function runFunctions() {
-  createJson().then(() => {
-    // createJson 함수가 완료된 후 몇초 기다린 후 update 함수 실행
-    setTimeout(update, 100);
-  });
-}
-
-// runFunctions 함수를 10분마다 반복 실행
-setInterval(runFunctions, 600000);
+async function runFunctions() {
+      const connection = mysqlConnection.init();
+      mysqlConnection.open(connection);
+    
+      try {
+        await createJson(connection);
+        await update(connection);
+      } catch (err) {
+        console.error('Error running functions:', err);
+      } finally {
+        connection.end();
+      }
+    }
+    
+    setInterval(runFunctions, 600000);
--- back/update.js
+++ back/update.js
@@ -1,51 +1,46 @@
 const mysqlConnection = require('./mysql');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const connection = mysqlConnection.init();
-mysqlConnection.open(connection);
 
-const update = () => {
-    fs.readFile('./update.json', 'utf8', (err, data) => {
-        if (err) {
-            console.log('JSON 파일 읽기 실패:', err);
-            return;
-        }
 
-        try {
-            const jsonData = JSON.parse(data);
+async function update(connection) {
+    mysqlConnection.open(connection);
+    try {
+        const data = await fs.readFile('./update.json', 'utf8');
+        const jsonData = JSON.parse(data);
 
-            // 이번 달에 해당하는 데이터 삭제
-            const deleteQuery = `DELETE FROM test2 WHERE MONTH(matched_at) = MONTH(CURRENT_DATE()) AND YEAR(matched_at) = YEAR(CURRENT_DATE())`;
-            connection.query(deleteQuery, (err, results) => {
+        // 이번 달에 해당하는 데이터 삭제
+        const deleteQuery = `DELETE FROM test2 WHERE MONTH(matched_at) = MONTH(CURRENT_DATE()) AND YEAR(matched_at) = YEAR(CURRENT_DATE())`;
+        connection.query(deleteQuery, (err, results) => {
                 if (err) {
-                    console.log('데이터베이스 쿼리 실행 실패:', err);
+                    console.log(' UD데이터베이스 쿼리 실행 실패:', err);
+                }
 
+                // 새로운 데이터 입력
+                for (let i = 0; i < jsonData.length; i++) {
+                    const item = jsonData[i];
+                    const { team_1, team_1_score, team_2, team_2_score, matched_at, match_is } = item;
 
+                    const query = `INSERT INTO test2 (team_1, team_1_score, team_2, team_2_score, matched_at, match_is) VALUES (${team_1}, ${team_1_score}, ${team_2}, ${team_2_score}, '${matched_at}', '${match_is}')`;
+
+                    connection.query(query, (err, results) => {
+                        if (err) {
+                            console.log('데이터베이스 쿼리 실행 실패:', err);
+                        }
+
+                        // 모든 쿼리가 완료되면 데이터베이스 연결 종료
+                        if (i === jsonData.length - 1) {
+                            connection.end();
+                        }
+                    })
                 }
             });
-
-            // 새로운 데이터 입력
-            for (const item of jsonData) {
-                const { team_1, team_1_score, team_2, team_2_score, matched_at, match_is } = item;
-
-                const query = `INSERT INTO test2 (team_1, team_1_score, team_2, team_2_score, matched_at, match_is) VALUES (${team_1}, ${team_1_score}, ${team_2}, ${team_2_score}, '${matched_at}', '${match_is}')`;
-
-                connection.query(query, (err, results) => {
-                    if (err) {
-                        console.log('데이터베이스 쿼리 실행 실패:', err);
-                    }
-                })
-            };
-
-            // 데이터베이스 연결 종료
-            connection.end();
         } catch (err) {
-            console.log('JSON 파일 파싱 실패:', err);
+            console.log('JSON 파일 읽기 실패:', err);
         }
-    });
-};
-
+    };
 // JSON 파일로부터 데이터를 데이터베이스에 입력
-update();
+
 
 module.exports = update;
