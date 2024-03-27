4cbf8d243aff84f7ae99beab85b56ac3a5ce747b
minjae <sapoon11@gmail.com>
2024-03-27 11:24:24 +0900

    로그인

--- back/login.js
+++ back/login.js
@@ -24,8 +24,6 @@ app.set('view engine', 'ejs'); // 보여주는 싯기를 ejs쓸거야 명시
 app.set('views', './views'); // views 라는 애들은, ./views 안에 있어
 app.use(bodyParser.urlencoded({extended:false}));
 
-
-
 const GOOGLE_CLIENT_ID = ''; // YOUR GOOGLE_CLIENT_ID
 const GOOGLE_CLIENT_SECRET = '' // YOUR GOOGLE_CLIENT_SECRET;
 const GOOGLE_LOGIN_REDIRECT_URI = 'http://localhost:3000/login/redirect';
@@ -40,6 +38,7 @@ app.get('/', (req, res) => {
         <a href="/signup">Sign up</a>
     `);
 });
+
 
 app.get('/login', (req, res) => {
     let url = 'https://accounts.google.com/o/oauth2/v2/auth';
@@ -60,13 +59,45 @@ app.get('/signup', (req, res) => {
 });
 
 
-app.get('/login/redirect', (req, res) => {
+app.get('/login/redirect', async (req, res) => {
     const { code } = req.query;
-    console.log(`code: ${code}`);
-    console.log('ok');
-    res.render('main')
 
+    const resp = await axios.post(GOOGLE_TOKEN_URL, {
+        code,
+        client_id: GOOGLE_CLIENT_ID,
+        client_secret: GOOGLE_CLIENT_SECRET,
+        redirect_uri: GOOGLE_LOGIN_REDIRECT_URI,
+        grant_type: 'authorization_code',
+    });
+
+    const resp2 = await axios.get(GOOGLE_USERINFO_URL, {
+        headers: {
+            Authorization: `Bearer ${resp.data.access_token}`,
+        },
+    });
+
+    const google_info = resp2.data;
+
+    console.log(google_info);
+    req.session.google_id = google_info.email; // 세션에 구글 아이디 저장
+
+    // 디비에서 코드값과 일치하는 구글 아이디를 조회하는 쿼리 실행
+    const sql = `SELECT user_id FROM users WHERE user_id = '${google_info.email}'`;
+    connection.query(sql, function(err, result) {
+        if (err) throw err;
+
+        if (result.length > 0) {
+            const user_id = result[0].user_id;
+
+            // 세션에 구글 아이디 저장
+            req.session.user_id = user_id;
+            console.log(`Google ID (${user_id}) stored in session.`);
+        }
+        
+        res.render('main');
+    });
 });
+
 
 app.get('/signup/redirect', async (req, res) => {
     const { code } = req.query;
@@ -89,6 +120,7 @@ app.get('/signup/redirect', async (req, res) => {
 
     console.log(google_info);
     req.session.email = google_info.email; // 세션에 이메일 주소 저장
+    
 
     res.render('npForm');
 });
@@ -96,8 +128,8 @@ app.get('/signup/redirect', async (req, res) => {
 app.post('/first/login.js', (req, res) => {
     const email = req.session.email; // 세션에서 이메일 주소 추출
     const { nick, phone } = req.body;
-
-    const sql = `insert into users(user_id,user_nick,user_phone,joined_at) values ('${email}','${nick}','${phone}',now())`;
+    
+    const sql = `INSERT INTO users (user_id, user_nick, user_phone, joined_at) VALUES ('${email}', '${nick}', '${phone}', NOW())`;
     connection.query(sql, function(err, result){
         if(err) throw err;
         console.log('자료 1개를 반환하였습니다.'); 
@@ -109,5 +141,4 @@ app.post('/first/login.js', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('server is running at 3000');
-});
+    console.log('server is running at 3000');});
