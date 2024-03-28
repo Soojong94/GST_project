e96dba0d47d4348e61771bbfd7561184af24df27
khbduino <khbroom1120@gmail.com>
2024-03-28 11:46:21 +0900

    1111

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Dashboard from './dashboard_logo';
 import AppBar from './appbar';
 import MyCalendar from './calendar';
-=======
-import Sidebar from '../src/sidebar-02/sidebar';
-import Routing from './routing/Routing';
->>>>>>> 030d63968b5019078be43594d8351d023a50508b
 
 function App() {
   return (
