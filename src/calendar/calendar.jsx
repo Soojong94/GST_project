08ce86a827f75a564f23b8ed59dc92b6f2bbdfe1
Soojong Kim <sujjong456@gmail.com>
2024-03-28 14:30:33 +0900

    0328 중간 커밋

--- src/calendar/calendar.jsx
+++ src/calendar/calendar.jsx
@@ -1,22 +1,44 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction'; // for selectable
 
 const MyCalendar = () => {
-  const calendarRef = useRef(null); // useRef 훅을 사용하여 FullCalendar 컴포넌트의 참조를 저장합니다.
+  const calendarRef = useRef(null);
+  const [events, setEvents] = useState([
+    { title: 'Event 1', date: '2022-03-01' },
+    { title: 'Event 2', date: '2022-03-02' }
+  ]);
 
   useEffect(() => {
     if (calendarRef.current) {
-      const calendar = calendarRef.current.getApi(); // FullCalendar API에 접근하기 위해 getApi 메서드를 사용합니다.
-      calendar.render(); // FullCalendar를 렌더링합니다.
+      const calendar = calendarRef.current.getApi(); 
+      calendar.render();
     }
   }, [calendarRef]);
 
+  const handleDateClick = (arg) => { // bind with an arrow function
+    const title = prompt('Enter Event Title');
+    if (title) {
+      setEvents([...events, { title, date: arg.date }]);
+    }
+  }
+
+  const handleEventClick = (arg) => {
+    if (window.confirm(`Are you sure you want to delete '${arg.event.title}'`)) {
+      arg.event.remove();
+    }
+  }
+
   return (
     <FullCalendar
-      ref={calendarRef} // FullCalendar 컴포넌트에 ref 속성을 추가하여 참조를 저장합니다.
-      plugins={[dayGridPlugin]} // 필요한 플러그인을 추가합니다.
-      initialView='dayGridMonth' // 초기 뷰를 설정합니다.
+      ref={calendarRef} 
+      plugins={[dayGridPlugin, interactionPlugin]}
+      initialView='dayGridMonth'
+      events={events}
+      dateClick={handleDateClick}
+      eventClick={handleEventClick}
+      selectable={true}
     />
   );
 };
--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -4,19 +4,18 @@ import Calendar from '../calendar/calendar';
 import TeamSubscriptions from '../team_subsc/team_logo'; 
 import ClanBoard from '../board/board'; 
 import TeamInfo from '../team_info/team_info'; 
-import Calendar2 from '../calendar2/calendar2';
 
 
 function Routing() {
   return (
     <Routes>
-      {/* <Route path = "/" element={<Calendar2 />}></Route>
-      <Route path = "Calendar2" element={<Calendar2 />}></Route> */}
-      {/* <Route path="/" element={<Calendar />} />
-      <Route path="/Calendar" element={<Calendar />} /> */}
+
+      <Route path="/" element={<Calendar />} />
+      <Route path="/Calendar" element={<Calendar />} />
       <Route path="/TeamSubscriptions" element={<TeamSubscriptions />} />
       <Route path="/ClanBoard" element={<ClanBoard />} />
       <Route path="/TeamInfo" element={<TeamInfo />} />
+      
     </Routes>
   );
 }
