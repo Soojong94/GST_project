9bd1080504a554e31667dff31cd9ccd5abe62c77
Soojong Kim <sujjong456@gmail.com>
2024-03-28 11:46:07 +0900

    0328

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Routing from './routing/Routing';
 import Calendar2 from  './calendar2/calendar2'
-=======
-<<<<<<< HEAD
-import Dashboard from './dashboard_logo';
-import AppBar from './appbar';
-import MyCalendar from './calendar';
-=======
-import Sidebar from '../src/sidebar-02/sidebar';
-import Routing from './routing/Routing';
->>>>>>> 030d63968b5019078be43594d8351d023a50508b
->>>>>>> 3c2afc836e4b83e5b8a4170d5c20ca501545944b
 
 function App() {
   return (
