48a07e9ee30127448db6f3a79f5c580480ec35a6
Soojong Kim <sujjong456@gmail.com>
2024-03-29 17:35:27 +0900

    0329 수종 완료 푸시

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
-import React, { useState } from 'react';
+// App.js
+import React from 'react';
 import './App.css';
 import Routing from './routing/Routing';
+import useCalendarEvent from './calendar/calendarEvent';
 
 function App() {
-
-  const [events, setEvents] = useState([
-    { title: 'Event 1', start: '2022-03-01' },
-    { title: 'Event 2', start: '2022-03-02' }
-  ]);
-
-  const handleNewSchedule = (newEvent) => {
-    console.log('handle New Schedule Fuc',events)
-    const { calendarType, title, startDate, endDate, time, location, description } = newEvent;
-    const start = `${startDate}T${time || '00:00'}`;
-    const end = endDate ? `${endDate}T${time || '00:00'}` : null;
-
-    const event = {
-      title,
-      start,
-      end,
-      calendarType,
-      location,
-      description
-    };
-
-    setEvents([...events, event]);
-  };
+  const { events, handleNewSchedule } = useCalendarEvent(); 
 
   return (
     <div>
-
-        <Routing events={events} handleNewSchedule={handleNewSchedule}/>
-
-    
+      <Routing events={events} handleNewSchedule={handleNewSchedule}/>
     </div>
   );
 }
