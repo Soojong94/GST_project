fa3d6f79c8d2d072b5a35ec2cdd8248705b0f64a
minjae <sapoon11@gmail.com>
2024-03-30 13:42:56 +0900

    zzzzzzzzzz

--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -5,13 +5,7 @@ import ClanBoard from '../board/board';
 import TeamInfo from '../team_info/team_info';
 import AddSchedule from '../AddSchedule/AddSchedule';
 import Calendar from '../calendar/calendar';
-<<<<<<< HEAD
 import Mypage from './../Mypage/Mypage';
-=======
-import Mypage from '../Mypage/Mypage';
-import Mainpage from '../mainpage/startbootstrap-grayscale-gh-pages/mainpage'
-import Board_1 from '../boardContent/boardContent';
->>>>>>> 002167940934ea62201480b70274b76bb97dde66
 
 
 
@@ -26,13 +20,8 @@ function Routing({ handleNewSchedule, events }) {
       <Route path="/ClanBoard" element={<ClanBoard />} />
       <Route path="/TeamInfo/:team_idx" element={<TeamInfo />} />
       <Route path="/AddSchedule/:date" element={<AddSchedule onScheduleAdded={handleNewSchedule} />} />
-<<<<<<< HEAD
-      <Route path='/Mypage' element={<Mypage />} />
 
-=======
-      <Route path="/Mypage" element={<Mypage />} />
-      <Route path="/Board_1" element={<Board_1 />} />
->>>>>>> 002167940934ea62201480b70274b76bb97dde66
+
 
     </Routes>
   );
